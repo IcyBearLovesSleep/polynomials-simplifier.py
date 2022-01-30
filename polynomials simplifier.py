42cd093c0c7adb1e47b4f4bb73b9f1e2bb29b017
Jarred-Zhang <95109943+Jarred-Zhang@users.noreply.github.com>
2022-01-29 23:09:24 -0800

    Delete polynomials simplifier.py

--- polynomials simplifier.py
+++
@@ -1,8 +0,0 @@
-import re
-def simplify(poly):
-    matches = re.findall(r'([+-]?)(\d*)([a-z]+)', poly)
-    expanded = [[int(i[0] + (i[1] if i[1] != "" else "1")), ''.join(sorted(i[2]))] for i in matches]
-    varb = sorted(list(set(i[1] for i in expanded)), key=lambda x: (len(x), x))
-    coef = {v:sum(i[0] for i in expanded if i[1] == v) for v in varb}
-    a=[str(coef[v]) + v if coef[v] not in [-1,1] else (str(coef[v]).replace('1','') + v) for v in varb if coef[v] != 0]
-    return '+'.join(a).replace('+-','-')
